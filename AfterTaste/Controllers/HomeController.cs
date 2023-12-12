02d15646fa2d7d06756cbec0bf34a8b39f06fbd3
Evander Prynz Villanueva <evanderprynz.villanueva.cics@ust.edu.ph>
2023-12-12 22:11:21 +0800

    fixed contact

--- AfterTaste/Controllers/HomeController.cs
+++ AfterTaste/Controllers/HomeController.cs
@@ -10,56 +10,56 @@ using System.Security.Claims;
 
 namespace AfterTaste.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly UserManager<User> _userManager;
-		private readonly AppDbContext _dbContext;
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly AppDbContext _dbContext;
+        private readonly ILogger<HomeController> _logger;
 
-		public HomeController(UserManager<User> userManager, AppDbContext dbContext, ILogger<HomeController> logger)
-		{
-			_userManager = userManager;
-			_dbContext = dbContext;
-			_logger = logger;
-		}
+        public HomeController(UserManager<User> userManager, AppDbContext dbContext, ILogger<HomeController> logger)
+        {
+            _userManager = userManager;
+            _dbContext = dbContext;
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public IActionResult Profile()
-		{
-			var user = _userManager.GetUserAsync(User).Result;
-			if (user == null)
-			{
-				// Handle user not found
-				return RedirectToAction("Index"); // Redirect to another action or handle appropriately
-			}
+        public IActionResult Profile()
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+            if (user == null)
+            {
+                // Handle user not found
+                return RedirectToAction("Index"); // Redirect to another action or handle appropriately
+            }
 
-			var createdRecipes = _dbContext.Recipes.Where(r => r.userId == user.Id).ToList();
+            var createdRecipes = _dbContext.Recipes.Where(r => r.userId == user.Id).ToList();
 
-			var favoriteRecipeIds = _dbContext.Favorites.Where(f => f.userId == user.Id).Select(f => f.recipeId).ToList();
+            var favoriteRecipeIds = _dbContext.Favorites.Where(f => f.userId == user.Id).Select(f => f.recipeId).ToList();
 
-			var favoriteRecipes = _dbContext.Recipes.Include(r => r.User) .Where(r => favoriteRecipeIds.Contains(r.recipeId))
-				.ToList();
+            var favoriteRecipes = _dbContext.Recipes.Include(r => r.User).Where(r => favoriteRecipeIds.Contains(r.recipeId))
+                .ToList();
 
-			var viewModel = new ProfileViewModel
-			{
-				CreatedRecipes = createdRecipes,
-				FavoriteRecipes = favoriteRecipes
-			};
+            var viewModel = new ProfileViewModel
+            {
+                CreatedRecipes = createdRecipes,
+                FavoriteRecipes = favoriteRecipes
+            };
 
-			return View(viewModel);
-		}
+            return View(viewModel);
+        }
 
 
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
 
         [Authorize]
         [HttpPost]
@@ -98,6 +98,33 @@ namespace AfterTaste.Controllers
                 return RedirectToAction("RecipeDetails", new { id = recipeId });
             }
         }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddContact(ContactUs newContact)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var currentUser = _userManager.GetUserAsync(User).Result;
+            if (currentUser == null)
+            {
+                // Handle user not found
+                return RedirectToAction("Index");
+            }
+
+            // Prefill the fullName and email fields with the currently logged-in user's info
+            newContact.fullName = $"{currentUser.Firstname} {currentUser.Lastname}";
+            newContact.email = currentUser.Email;
+
+            _dbContext.ContactUs.Add(newContact);
+            _dbContext.SaveChanges();
+
+            // Redirect to the index page or wherever appropriate
+            return RedirectToAction("Index");
+        }
+
+
 
     }
 }
